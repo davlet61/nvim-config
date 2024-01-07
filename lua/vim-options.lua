5ac59aa183b32cd1a511aa8be5271cf2d16faa9b
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-01-07 23:16:41 +0100

    adds highlight on yank

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,28 @@ vim.g.mapleader = " "
 
 
 require("keymaps")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("TextYankPost", {
+  desc = "Highlight yanked text",
+  group = augroup("highlightyank", { clear = true }),
+  pattern = "*",
+  callback = function() vim.highlight.on_yank() end,
+})
+
+-- autocmd("ColorScheme", {
+--   desc = "Load custom highlights from user configuration",
+--   group = augroup("astronvim_highlights", { clear = true }),
+--   callback = function()
+--     if vim.g.colors_name then
+--       for _, module in ipairs { "init", vim.g.colors_name } do
+--         for group, spec in pairs(astronvim.user_opts("highlights." .. module)) do
+--           vim.api.nvim_set_hl(0, group, spec)
+--         end
+--       end
+--     end
+--     astroevent("ColorScheme", false)
+--   end,
+-- })
