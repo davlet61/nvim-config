35ef83ba09387217bd736651e48ec91616e73e91
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-01-08 01:09:33 +0100

    proper utils

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+--- Check if a plugin is defined in lazy. Useful with lazy loading when a plugin is not necessarily loaded yet
+---@param plugin string The plugin to search for
+---@return boolean available # Whether the plugin is available
+function M.is_available(plugin)
+  local lazy_config_avail, lazy_config = pcall(require, "lazy.core.config")
+  return lazy_config_avail and lazy_config.spec.plugins[plugin] ~= nil
+end
+
+--- Check if a buffer is valid
+---@param bufnr number? The buffer to check, default to current buffer
+---@return boolean # Whether the buffer is valid or not
+function M.is_valid(bufnr)
+  if not bufnr then bufnr = 0 end
+  return vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].buflisted
+end
 
 --- Get highlight properties for a given highlight name
 ---@param name string The highlight group name
@@ -25,4 +40,48 @@ function M.get_hlgroup(name, fallback)
   return fallback or {}
 end
 
+--- Toggle a user terminal if it exists, if not then create a new one and save it
+---@param opts string|table A terminal command string or a table of options for Terminal:new() (Check toggleterm.nvim documentation for table format)
+function M.toggle_term_cmd(opts)
+  local terms = {}
+  -- if a command string is provided, create a basic table for Terminal:new() options
+  if type(opts) == "string" then opts = { cmd = opts, hidden = true } end
+  local num = vim.v.count > 0 and vim.v.count or 1
+  -- if terminal doesn't exist yet, create it
+  if not terms[opts.cmd] then terms[opts.cmd] = {} end
+  if not terms[opts.cmd][num] then
+    if not opts.count then opts.count = vim.tbl_count(terms) * 100 + num end
+    if not opts.on_exit then opts.on_exit = function() terms[opts.cmd][num] = nil end end
+    terms[opts.cmd][num] = require("toggleterm.terminal").Terminal:new(opts)
+  end
+  -- toggle the terminal
+  terms[opts.cmd][num]:toggle()
+end
+
+--- Close a given buffer
+---@param bufnr? number The buffer to close or the current buffer if not provided
+---@param force? boolean Whether or not to foce close the buffers or confirm changes (default: false)
+function M.close(bufnr, force)
+  if not bufnr or bufnr == 0 then bufnr = vim.api.nvim_get_current_buf() end
+  if M.is_available "mini.bufremove" and M.is_valid(bufnr) and #vim.t.bufs > 1 then
+    if not force and vim.api.nvim_get_option_value("modified", { buf = bufnr }) then
+      local bufname = vim.fn.expand "%"
+      local empty = bufname == ""
+      if empty then bufname = "Untitled" end
+      local confirm = vim.fn.confirm(('Save changes to "%s"?'):format(bufname), "&Yes\n&No\n&Cancel", 1, "Question")
+      if confirm == 1 then
+        if empty then return end
+        vim.cmd.write()
+      elseif confirm == 2 then
+        force = true
+      else
+        return
+      end
+    end
+    require("mini.bufremove").delete(bufnr, force)
+  else
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
+    vim.cmd(("silent! %s %d"):format((force or buftype == "terminal") and "bdelete!" or "confirm bdelete", bufnr))
+  end
+end
 return M
