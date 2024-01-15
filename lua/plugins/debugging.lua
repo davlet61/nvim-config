9902821fb8fd7ea4232bf230d66acff6059d8ea4
Dovlat <dovlat93@gmail.com>
2024-01-16 00:05:39 +0100

    dap

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,31 +1,52 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"leoluz/nvim-dap-go",
-		"rcarriga/nvim-dap-ui",
-	},
-	config = function()
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "leoluz/nvim-dap-go",
+    "rcarriga/nvim-dap-ui",
+  },
+  config = function()
     require("dapui").setup()
-		require("dap-go").setup()
+    require("dap-go").setup()
 
-		local dap, dapui = require("dap"), require("dapui")
+    local dap, dapui = require("dap"), require("dapui")
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "127.0.0.1",
+      port = 8123,
+      executable = {
+        command = "js-debug-adapter",
+      }
+    }
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    for _, language in ipairs { "typescript", "javascript" } do
+      dap.configurations[language] = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          runtimeExecutable = "node",
+        },
+      }
+    end
 
-		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-	end,
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<Leader>dr", ":DapContinue<CR>")
+    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+  end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         auto_install = true,
-        ensure_installed = { "lua_ls", "graphql", "tsserver", "html", "gopls" },
+        ensure_installed = { "lua_ls", "graphql", "tsserver", "html", "gopls", "js-debug-adapter" },
       })
     end,
   },
