ba94cae0e5de64543c5c901a01b5a1a5f0f51e83
Dovlat <dovlat93@gmail.com>
2024-01-15 23:25:51 +0100

    go lsp

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -23,6 +23,9 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.golines
 			},
 		})
 
