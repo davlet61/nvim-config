4d7100c7f8f239826d4f1a0c7ee3e1e4fccfa2d8
Dovlat <dovlat93@gmail.com>
2024-01-16 22:24:40 +0100

    remove mini bufremove

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    version = "*",
-    config = function()
-      require("mini.move").setup({
-        keys = {
-          { "<M-l>", mode = { "n", "v" } },
-          { "<M-k>", mode = { "n", "v" } },
-          { "<M-j>", mode = { "n", "v" } },
-          { "<M-h>", mode = { "n", "v" } },
-        },
-      })
-      require("mini.bufremove").setup({})
-    end,
-  },
+	{
+		"echasnovski/mini.nvim",
+		version = "*",
+		config = function()
+			require("mini.move").setup({
+				keys = {
+					{ "<M-l>", mode = { "n", "v" } },
+					{ "<M-k>", mode = { "n", "v" } },
+					{ "<M-j>", mode = { "n", "v" } },
+					{ "<M-h>", mode = { "n", "v" } },
+				},
+			})
+			-- require("mini.bufremove").setup({})
+		end,
+	},
 }
