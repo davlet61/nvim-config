f4b4e71cbc8e666c4f7472da7b521d8e631940e9
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-01-17 10:17:55 +0100

    debug

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -24,27 +24,27 @@ return {
     for _, language in ipairs({ "typescript", "javascript" }) do
       dap.configurations[language] = {
         {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Launch Current File (pwa-node)',
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch Current File (pwa-node)",
           cwd = "${workspaceFolder}", -- vim.fn.getcwd(),
-          args = { '${file}' },
+          args = { "${file}" },
           sourceMaps = true,
-          protocol = 'inspector',
+          protocol = "inspector",
         },
         {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Launch Current File (Typescript)',
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch Current File (Typescript)",
           cwd = "${workspaceFolder}",
-          runtimeArgs = { '--loader=ts-node/esm' },
+          runtimeArgs = { "--import tsx" },
           program = "${file}",
-          runtimeExecutable = 'node',
+          runtimeExecutable = "node",
           -- args = { '${file}' },
           sourceMaps = true,
-          protocol = 'inspector',
+          protocol = "inspector",
           outFiles = { "${workspaceFolder}/**/**/*", "!**/node_modules/**" },
-          skipFiles = { '<node_internals>/**', 'node_modules/**' },
+          skipFiles = { "<node_internals>/**", "node_modules/**" },
           resolveSourceMapLocations = {
             "${workspaceFolder}/**",
             "!**/node_modules/**",
@@ -61,12 +61,12 @@ return {
     end
 
     -- !!! Fix auto-closing issue
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    -- dap.listeners.before.event_terminated.dapui_config = function()
+    --   dapui.close()
+    -- end
+    -- dap.listeners.before.event_exited.dapui_config = function()
+    --   dapui.close()
+    -- end
 
     vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
     vim.keymap.set("n", "<Leader>dr", ":DapContinue<CR>")
